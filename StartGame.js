1ad4a3a16af431251e3c66566c8e5b511f65d2fa
Thuan Nguyen <235thuan@gmail.com>
2019-06-08 15:00:27 +0700

    .

--- StartGame.js
+++ StartGame.js
@@ -1,8 +1,8 @@
-let value_Width= 500;
-let value_Height= 400;
-let myBasement,myCoordinate,myArrow;
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
+let value_Width= 500;
+let value_Height= 400;
+let myBasement,myCoordinate,myPlanetA,myPlanetB,myBomb;
 let sizeXbasement=50;
 let sizeYbasement=50;
 let coorXbasement=value_Height/2-25;
@@ -11,20 +11,24 @@ let coorYbasement=value_Height/2-25;
 function startGame() {
     myBasement = new Basement(coorXbasement, coorYbasement, sizeXbasement, sizeYbasement);
     myCoordinate= new Coordinate(100,100,10,10);
+    myPlanetA= new PlanetA(100,100,50,50);
+    myPlanetB= new PlanetB(100,100,50,50);
+    myBomb= new Bomb(coorXbasement+15,coorYbasement+15,20,20);
     myCoordinate.draw();
     myBasement.draw();
-    myGameArea.start();
+    myPlanetA.draw();
+    myPlanetB.draw();
+    myBomb.draw();
+    // myGameArea.start();
 }
 
-let myGameArea = {
-
-    start: function () {
-        this.canvas.width = value_Width;
-        this.canvas.height = value_Height;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    },
-};
+// let myGameArea = {
+//
+//     start: function () {
+//         this.context = this.canvas.getContext("2d");
+//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//     },
+// };
 
 function Basement (x,y,width,height) {
     this.x=x;
@@ -36,6 +40,8 @@ function Basement (x,y,width,height) {
     this.draw = function() {
         let x=this.x;
         let y=this.y;
+        let width=this.width;
+        let height=this.height;
         ctx.shadowBlur = 20;
         ctx.shadowColor = "yellow";
         ctx.drawImage(this.image, x, y, width, height);
@@ -53,15 +59,15 @@ function Coordinate(x,y,width,height) {
         ctx.shadowColor = "yellow";
         //oy
         ctx.moveTo(value_Height/2,0);
-        ctx.lineTo(value_Height/2,value_Width*1);
+        ctx.lineTo(value_Height/2,500);
         //ox
         ctx.moveTo(0,value_Height/2);
         ctx.lineTo(value_Width,value_Height/2);
         //arrow
-        ctx.moveTo(380,190);
-        ctx.lineTo(value_Width,value_Height-150);
-        ctx.moveTo(380,210);
-        ctx.lineTo(value_Width,value_Height-250);
+        ctx.moveTo(480,210);
+        ctx.lineTo(value_Width,value_Height-200);
+        ctx.moveTo(480,190);
+        ctx.lineTo(value_Width,value_Height-200);
         ctx.moveTo(190,20);
         ctx.lineTo(value_Height/2,0);
         ctx.moveTo(210,20);
@@ -77,7 +83,38 @@ function PlanetA(x,y,width,height) {
     this.width=width;
     this.height=height;
     this.image = new Image();
-    this.image.src= "explosive3.png"; //link anh
+    this.image.src= "explosive4.png"; //link anh
+    this.draw = function() {
+        let x=this.x*Math.random();
+        let y=this.y*Math.random();
+        ctx.shadowBlur = 20;
+        ctx.shadowColor = "yellow";
+        ctx.drawImage(this.image, x, y, width, height);
+    }
+}
+function PlanetB(x,y,width,height) {
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.image = new Image();
+    this.image.src= "explosive4.png"; //link anh
+    this.draw = function() {
+        let x=this.x*Math.random();
+        let y=this.y*Math.random();
+        ctx.shadowBlur = 20;
+        ctx.shadowColor = "yellow";
+        ctx.drawImage(this.image, x, y, width, height);
+    }
+}
+
+function Bomb(x,y,width,height) {
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.image = new Image();
+    this.image.src= "explosive5.png"; //link anh
     this.draw = function() {
         let x=this.x;
         let y=this.y;
@@ -85,8 +122,5 @@ function PlanetA(x,y,width,height) {
         ctx.shadowColor = "yellow";
         ctx.drawImage(this.image, x, y, width, height);
     }
-
 }
-
-
 
