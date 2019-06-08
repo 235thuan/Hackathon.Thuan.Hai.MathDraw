b18f917154405a64eefc338339d4beeb5a1fb774
Thuan Nguyen <235thuan@gmail.com>
2019-06-08 14:04:55 +0700

    .

--- StartGame.js
+++ StartGame.js
@@ -1,17 +1,18 @@
 let value_Width= 500;
 let value_Height= 400;
-let myBasement,myCoordinateY,myCoordinateX,myArrow;
+let myBasement,myCoordinate,myArrow;
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
+let sizeXbasement=50;
+let sizeYbasement=50;
+let coorXbasement=value_Height/2-25;
+let coorYbasement=value_Height/2-25;
+
 function startGame() {
-    myBasement = new Basement(value_Height/2-25, value_Height/2-25, 50, 50);
-    myCoordinateY= new CoordinateY(100,100,10,10);
-    myCoordinateX= new CoordinateX(100,100,10,10);
-    myArrow=new Arrow(100,100,10,10);
-    myCoordinateY.draw();
-    myCoordinateX.draw();
+    myBasement = new Basement(coorXbasement, coorYbasement, sizeXbasement, sizeYbasement);
+    myCoordinate= new Coordinate(100,100,10,10);
+    myCoordinate.draw();
     myBasement.draw();
-    myArrow.draw();
     myGameArea.start();
 }
 
@@ -41,7 +42,7 @@ function Basement (x,y,width,height) {
     }
 }
 
-function CoordinateY(x,y,width,height) {
+function Coordinate(x,y,width,height) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -50,42 +51,42 @@ function CoordinateY(x,y,width,height) {
         ctx.beginPath();
         ctx.shadowBlur = 20;
         ctx.shadowColor = "yellow";
+        //oy
         ctx.moveTo(value_Height/2,0);
         ctx.lineTo(value_Height/2,value_Width*1);
-        ctx.strokeStyle="white";
-        ctx.stroke();
-    }
-}
-
-function CoordinateX(x,y,width,height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.draw = function () {
-        ctx.beginPath();
-        ctx.shadowBlur = 20;
-        ctx.shadowColor = "yellow";
+        //ox
         ctx.moveTo(0,value_Height/2);
         ctx.lineTo(value_Width,value_Height/2);
+        //arrow
+        ctx.moveTo(380,190);
+        ctx.lineTo(value_Width,value_Height-150);
+        ctx.moveTo(380,210);
+        ctx.lineTo(value_Width,value_Height-250);
+        ctx.moveTo(190,20);
+        ctx.lineTo(value_Height/2,0);
+        ctx.moveTo(210,20);
+        ctx.lineTo(value_Height/2,0);
         ctx.strokeStyle="white";
         ctx.stroke();
     }
 }
 
-function Arrow(x,y,width,height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.draw = function () {
-        ctx.beginPath();
+function PlanetA(x,y,width,height) {
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.image = new Image();
+    this.image.src= "explosive3.png"; //link anh
+    this.draw = function() {
+        let x=this.x;
+        let y=this.y;
         ctx.shadowBlur = 20;
         ctx.shadowColor = "yellow";
-        ctx.moveTo(400,200);
-        ctx.lineTo(value_Width,value_Height-150);
-        ctx.strokeStyle="white";
-        ctx.stroke();
+        ctx.drawImage(this.image, x, y, width, height);
     }
+
 }
+
+
 
